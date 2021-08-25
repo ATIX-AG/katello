3b5e690ed525e1fc4b4ceb34cbbdc94e4fcc8984
Bernhard Suttner <suttner@atix.de>
2021-08-25 11:48:08 +0200

    Enable traces for debian should install katello-host-tools-tracer for deb

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/content-hosts/content/content-host-debs-actions.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/content-hosts/content/content-host-debs-actions.controller.js
@@ -4,15 +4,20 @@
  *
  * @requires $scope
  * @requires translate
+ * @requires $location
  *
  * @description
  *   Provides the functionality for the content host deb package actions.
  */
 angular.module('Bastion.content-hosts').controller('ContentHostDebsActionsController',
-    ['$scope', 'translate', function ($scope, translate) {
+    ['$scope', 'translate', '$location', function ($scope, translate, $location) {
         // Labels so breadcrumb strings can be translated
         $scope.label = translate('Deb Package Actions');
-
+        var packageName = $location.search().package_name;
         $scope.packageAction = {actionType: 'packageInstall'};  // default to packageInstall
+
+        if (packageName) {
+            $scope.packageAction.term = packageName;
+        }
     }
 ]);
