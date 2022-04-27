a405ad701ff8c93908e19348cdcbe8e2f74bce24
Markus Bucher <bucher@atix.de>
2022-04-27 17:35:21 +0200

    Use advanced copy for deb
    Heavily 'inspired' by the rpm-implementation
    Refs OR-3108
    
    (cherry picked from commit 2d68803bbd235f961cefae57c5fd2b603990cb1c)

--- app/services/katello/pulp3/api/apt.rb
+++ app/services/katello/pulp3/api/apt.rb
@@ -4,8 +4,16 @@ module Katello
   module Pulp3
     module Api
       class Apt < Core
+        def self.copy_class
+          PulpDebClient::Copy
+        end
+
         def publication_verbatim_class
           PulpDebClient::DebVerbatimPublication
+        end
+
+        def copy_api
+          PulpDebClient::DebCopyApi.new(api_client)
         end
 
         def publications_verbatim_api
