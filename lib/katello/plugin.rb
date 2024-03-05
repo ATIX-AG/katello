3641a92c361f2c0b5a895258c38f99e3abd8f7fa
Bernhard Suttner <sbernhard@users.noreply.github.com>
2024-03-05 15:37:44 +0100

    Fixes #37191 - Add REX feature for uploading package profile (#10900)
    (cherry picked from commit 559f43c6ad81168ae396b61509d828129ceae6ec)

--- lib/katello/plugin.rb
+++ lib/katello/plugin.rb
@@ -704,6 +704,7 @@ Foreman::Plugin.register :katello do
     RemoteExecutionFeature.register(:katello_errata_install_by_search, N_("Katello: Install errata by search query"), :description => N_("Install errata using scoped search query"), :provided_inputs => ['Errata search query'])
     RemoteExecutionFeature.register(:katello_service_restart, N_("Katello: Service Restart"), :description => N_("Restart Services via Katello interface"), :provided_inputs => ['helpers'])
     RemoteExecutionFeature.register(:katello_host_tracer_resolve, N_("Katello: Resolve Traces"), :description => N_("Resolve traces via Katello interface"), :provided_inputs => ['ids'])
+    RemoteExecutionFeature.register(:katello_upload_profile, N_("Katello: Upload Profile"), :description => N_("Upload package / repos profile"), :provided_inputs => [])
     RemoteExecutionFeature.register(:katello_module_stream_action, N_("Katello: Module Stream Actions"),
                                     :description => N_("Perform a module stream action via Katello interface"),
                                     :provided_inputs => ['action', 'module_spec', 'options'])
