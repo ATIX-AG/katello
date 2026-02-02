2743d1d589a241b2d4be19799d304855555e698b
Jeremy Lenz <jlenz@redhat.com>
2026-02-02 14:28:08 -0500

    Fixes #39052 - Prevent infinite loop when editing AK description
    (cherry picked from commit 12b9e221c356b913bccf503333c549b48f9da3c6)
    
    Fixes OR-6775

--- webpack/scenes/ActivationKeys/Details/components/CVEDetailsCard.js
+++ webpack/scenes/ActivationKeys/Details/components/CVEDetailsCard.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { propsToCamelCase } from 'foremanReact/common/helpers';
+import { isEqual } from 'lodash';
 import { CVEDetailsBareCard } from '../../../../components/extensions/HostDetails/Cards/ContentViewDetailsCard/ContentViewDetailsCard';
 
 const getAKDetailsFromDOM = (node) => {
@@ -12,13 +13,25 @@ const getAKDetailsFromDOM = (node) => {
 export const CVEDetailsCard = () => { // used as foreman-react-component, takes no props
   const akDetailsNode = useRef(document.getElementById('ak-cve-details')).current;
   const [akDetails, setAkDetails] = useState(getAKDetailsFromDOM(akDetailsNode));
+  const observedAkDetailsAttributes = new Set([
+    'id',
+    'organization_id',
+    'content_view_environments',
+  ]);
 
   const observer = new MutationObserver((mutationsList) => {
     // eslint-disable-next-line no-restricted-syntax
     for (const mutation of mutationsList) {
       if (mutation.type === 'attributes' && mutation.attributeName.startsWith('data-')) {
         akDetailsNode.current = document.getElementById('ak-cve-details');
-        setAkDetails(getAKDetailsFromDOM(akDetailsNode));
+        const newDetails = getAKDetailsFromDOM(akDetailsNode);
+        // eslint-disable-next-line no-restricted-syntax
+        for (const key of Object.keys(newDetails)) {
+          if (!isEqual(akDetails[key], newDetails[key]) && observedAkDetailsAttributes.has(key)) {
+            setAkDetails(newDetails);
+            return;
+          }
+        }
       }
     }
   });
